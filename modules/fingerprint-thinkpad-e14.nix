cdbcb6ad0291d6ee755c62efb5a44e2246f74c4c
Andrea Ciliberti <meziu210@icloud.com>
2026-08-16 19:45:06 +0200

    Giving up on fingerprint for now

--- modules/fingerprint-thinkpad-e14.nix
+++ modules/fingerprint-thinkpad-e14.nix
@@ -1,11 +1,11 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
-  services.fprintd = {
-    enable = true;
-    tod = {
-      enable = true;
-      driver = pkgs.libfprint-2-tod1-goodix-550a;
-    };
-  };
+  #services.fprintd = {
+  #  enable = true;
+  #  tod = {
+  #    enable = true;
+  #    driver = pkgs.libfprint-2-tod1-goodix-550a;
+  #  };
+  #};
 }
